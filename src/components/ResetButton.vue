725f345c80ec5457a39200a8e6a088859bd48799
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2020-08-15 11:19:59 +0900

    ライフサイクルフック機能追加

---
+++ src/components/ResetButton.vue
@@ -0,0 +1,30 @@
+<template>
+    <button @click="onClick">Reset</button>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Emit,  Vue} from "vue-property-decorator";
+
+    @Component
+    export default class MyButton extends Vue{
+        private initialValue!: string;
+
+        @Prop()
+        public value!: string;
+
+        //ライフサイクルフック
+        public created(){
+            this.initialValue = this.value;
+        }
+
+        @Emit()
+        public input(value: string){
+        }
+
+        public onClick(){
+            this.input(this.initialValue);
+        }
+    }
+</script>
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <MyButton greet="Hello"></MyButton>
+    <p>{{ greetText }}</p>
+    <p>
+      <MyButton :greet="greetText" @click="onMyButtonClicked"></MyButton>
+    </p>
+    <p>
+      <ResetButton initialValue="Hello" v-model="greetText"></ResetButton>
+    </p>
   </div>
 </template>
 
-<script lamg="ts">
+<script lang="ts">
   // @ is an alias to /src
   import { Component, Vue} from 'vue-property-decorator';
   import MyButton from  '@/components/MyButton.vue'
+  import ResetButton from  '@/components/ResetButton.vue'
 
-  @Component({
-    components: {
-      MyButton,
-  },
+    @Component({
+      components: {
+        MyButton,
+        ResetButton
+    },
   })
-    export default class Home extends Vue {}
+  export default class Home extends Vue {
+    //↓VScodeで「不要な型宣言」でエラーが出てしまう
+      public greetText: string = "Hello";
+
+      public onMyButtonClicked(){
+          this.greetText = "こんにちは";
+      }
+  }
 </script>
